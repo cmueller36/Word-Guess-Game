bfce6ba2df54a33b7457b3edd8c47ce676009016
Christopher Mueller <christopher.mueller@cpxprojects.com>
2018-05-03 16:07:40 -0400

    added hints for the words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 //declare the words for the computer to choose from 10 choices
 var computerChoices = ["car", "plane", "train", "cloud", "road"];
+var wordHint =["I have 4 wheels","I fly in the air","I ride on rails","I am in the blue sky","I am black"];
 
 var blankANDguessed = [];
 
@@ -9,6 +10,7 @@ var userWin = 0;
 var userLoss = 0;
 var remainingGuess = 15;
 var userGuess = [];
+var hint = "";
 
 
 
@@ -26,7 +28,12 @@ console.log(arrComputerWord);
 function startGame() {
     for (var i = 0; i < computerWord.length; i++) {
         blankANDguessed.push("_");
-    }
+    };
+
+    var hint = wordHint[computerChoices.indexOf(computerWord)];
+    var html1 = 
+    "<p> Hint: "+ hint + "</p>";
+    document.querySelector("#userHint").innerHTML = html1;
 }
 
 
@@ -42,7 +49,12 @@ startbutton.onclick = function (event) {
 document.onkeyup = function(event){
     //turn user guess to lower case and store in variable
     var userKeyGuess = event.key.toLowerCase();
+    //push the user guess into the variable userGuess
     userGuess.push(userKeyGuess);
+    //display the user guesses
+    var html =
+    "<p> You have chose: "+ userGuess + "</p>";
+    document.querySelector("#guesses").innerHTML = html;
     //check to see if word is computer word
     for(var i =0; i < computerWord.length; i++){
         if(computerWord[i] == userKeyGuess){
